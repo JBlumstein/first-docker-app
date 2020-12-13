09e570405bc16676b0488a802cd4ce6b36a75765
Jonah Blumstein <jonahblumstein@Jonahs-MacBook-Pro-2.local>
2020-12-13 17:31:04 -0500

    reassign some env to arg

--- container/local_build_docker.sh
+++ container/local_build_docker.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-docker build -t jonahblumstein/myfirstapp .
-docker run -it --log-opt mode=non-blocking --log-opt max-buffer-size=4m -e AWS_KEY=$AWSAccessKeyId -e AWS_SECRET=$AWSSecretKey --name myfirstapp jonahblumstein/myfirstapp
+docker build --build-arg aws_key=$AWSAccessKeyId --build-arg aws_secret=$AWSSecretKey -t jonahblumstein/bookrecoapp .
+docker run -it --log-opt mode=non-blocking --log-opt max-buffer-size=4m --name bookrecoapp jonahblumstein/bookrecoapp
