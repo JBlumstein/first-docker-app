b38668bd7612eba3deffe0f0ead869a8a0e9d9c8
Jonah Blumstein <jonahblumstein@Jonahs-MacBook-Pro-2.local>
2020-12-15 02:28:35 -0500

    all working now:

--- deploy_and_run_fargate.sh
+++ deploy_and_run_fargate.sh
@@ -3,19 +3,23 @@ source config
 
 # get datetime
 dtnow="`date +%Y%m%d%H%M%S`"
+container_name_dt=$container_name-$dtnow
 
 # create policy if none exists
 aws iam --region $aws_region create-role --role-name $role_name --assume-role-policy-document file://task-execution-assume-role.json
 aws iam --region $aws_region attach-role-policy --role-name $role_name --policy-arn arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy
 
-# create a cluster
-aws ecs create-cluster --cluster-name $cluster_name --capacity-providers FARGATE
-
 # fill out task definition template
-python3 update_task_definition.py $dtnow
+python3 update_task_definition.py $container_name_dt
+
+# create a cluster
+aws ecs create-cluster --cluster-name $cluster_name --capacity-providers FARGATE 
+
+# create a log group
+aws logs create-log-group --log-group-name $log_group_name
 
 # register a task definition
-aws ecs register-task-definition --cli-input-json file://updated-task-definition.json
+aws ecs register-task-definition --cli-input-json file://updated_task_definition.json
 
-# start task in cluster
-aws ecs start-task 
+# run task in cluster
+aws ecs run-task --launch-type FARGATE --task-definition $task_definition_name --cluster arn:aws:ecs:$aws_region:$aws_account_id:cluster/$cluster_name --network-configuration "awsvpcConfiguration={subnets=[$subnet1,$subnet2],securityGroups=[$security_group],assignPublicIp=ENABLED}"
--- update_task_definition.py
+++ update_task_definition.py
@@ -3,31 +3,52 @@ import os
 import sys
 from datetime import datetime
 
+def parse_config(file_name='config'):
+    conf = {}
+    with open(file_name) as fp:
+        for line in fp:
+            key, val = line.strip().split('=', 1)
+            conf[key] = val
+    return conf
+
+# read in container name with dt
+container_name = str(sys.argv[1])
+
+# parse config
+env_vars = parse_config()
+
 # fill in environment variables
-role_name = os.environ['role_name']
-aws_account_id = os.environ['aws_account_id']
-aws_region = os.environ['aws_region']
-repo_image = os.environ['repo_image']
-repo_name = os.environ['repo_name']
-repo_tag = os.environ['repo_tag']
-task_definition_name = os.environ['task_definition_name']
+role_name = env_vars['role_name']
+aws_account_id = env_vars['aws_account_id']
+aws_region = env_vars['aws_region']
+repo_image = env_vars['repo_image']
+repo_name = env_vars['repo_name']
+repo_tag = env_vars['repo_tag']
+task_definition_name = env_vars['task_definition_name']
+log_group_name = env_vars['log_group_name']
+role_name = env_vars['role_name']
 
-# use current datetime in naming
-dt_now = str(sys.argv[1])
-
-# read in task definition template
-with open('task_definition_template.json') as template_file: 
-    template = json.load(template_file)
-
-# fill in template
-template['executionRoleArn'] = template['executionRoleArn'].replace('account_id', aws_account_id).replace('role_name', role_name)
-template['containerDefinitions'][0]['image'] = template['containerDefinitions'][0]['image'].replace('account_id', aws_account_id).replace('repo_image', repo_image).replace('repo_name', repo_name).replace('repo_tag', repo_tag)
-template['containerDefinitions'][0]['name'] = template['containerDefinitions'][0]['name'] + '-' + dt_now
-template['taskRoleArn'] = template['taskRoleArn'].replace('account_id', aws_account_id).replace('role_name', role_name)
-template['taskDefinitionArn'] = template['taskDefinitionArn'].replace('aws_region', aws_region).replace('account_id', aws_account_id).replace('task_definition_name', task_definition_name)
-template['family'] = task_definition_name
-template['containerDefinitions'][0]['logConfiguration']['options']['awslogs-group'] = template['containerDefinitions'][0]['logConfiguration']['options']['awslogs-group'].replace('task_definition_name', task_definition_name)
-template['containerDefinitions'][0]['logConfiguration']['options']['awslogs-region'] = aws_region
+template = {
+	"executionRoleArn": f"arn:aws:iam::{aws_account_id}:role/{role_name}",
+	"containerDefinitions": [{
+		"logConfiguration": {
+			"logDriver": "awslogs",
+			"options": {
+				"awslogs-group": log_group_name,
+				"awslogs-region": aws_region,
+				"awslogs-stream-prefix": "ecs"
+			}
+		},
+		"image": f"{repo_image}/{repo_name}:{repo_tag}",
+		"name": container_name
+	}],
+	"memory": "8192",
+	"taskRoleArn": f"arn:aws:iam::{aws_account_id}:role/{role_name}",
+	"family": task_definition_name,
+	"requiresCompatibilities": ["FARGATE"],
+	"networkMode": "awsvpc",
+	"cpu": "2048"
+}
 
 # write out update task definition
 with open('updated_task_definition.json', 'w') as outfile:
